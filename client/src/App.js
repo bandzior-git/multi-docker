4d03f166a4e56f6070024a77582bdbacc1047426
bandzior-git <balcer77@gmail.com>
2021-09-15 11:44:51 +0200

    borrowed stuff from course

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            My fib app or whatever
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
